e0b4e690e9679505e3e4c4a0ab21dd3a28a7abc5
Florin Barbisch <florin.barbisch@gmail.com>
2021-11-28 19:05:38 +0100

    Added: kiosk service

---
+++ kiosk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+sleep 30
+
+/usr/bin/python /home/pi/FHNW_Wettermonitor/weather_app/main.py &
