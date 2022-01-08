bug fix (forecast) / accuracy calculation

weather_app/Logger.py
@@ -20,13 +20,14 @@ from flaskwebgui import FlaskUI
 from flask_influxdb import InfluxDB
 import weatherimport
 import threading
+from dateutil.relativedelta import relativedelta
 
 
 
 wd.init()
 
-weatherimport.generate_today_graphs()
-weatherimport.generate_last_7_days_graphs()
+""" weatherimport.generate_today_graphs()
+weatherimport.generate_last_7_days_graphs() """
 
 #print(wd.get_all_measurements("mythenquai", "1d"))
 
@@ -102,76 +103,81 @@ weatherimport.generate_last_7_days_graphs()
 #print(wd.construct_day_vector(table_day, [(0, 5, 1), (0, 3, 1)]))
 #print(wd.construct_day_vector_old(table_day1, [(0, 5, 1), (0, 3, 1)]))
 
-# def generateRandomDate(startDate, endDate) -> datetime:
-#     time_between_dates = endDate - startDate
-#     days_between_dates = time_between_dates.days
+# compare nearest neighbour with date last year
+""" def generateRandomDate(startDate, endDate) -> datetime:
+    time_between_dates = endDate - startDate
+    days_between_dates = time_between_dates.days
 
-#     random_number_of_days = random.randrange(days_between_dates)
-#     return startDate + timedelta(days=random_number_of_days)
+    random_number_of_days = random.randrange(days_between_dates)
+    return startDate + timedelta(days=random_number_of_days)
 
-# numberOfDates = 100
-# startDate = datetime(2008, 1, 1)
-# endDate = datetime(2020, 12, 1)
+numberOfDates = 100
+startDate = datetime(2009, 1, 1)
+endDate = datetime(2020, 12, 1)
 
-# dates = []
-# random.seed(53165441847244654)
-# for i in range(0, numberOfDates):
-#     date = generateRandomDate(startDate, endDate)
-#     dates.append(datetime(date.year, date.month, date.day))
+dates = []
+random.seed(53165441847244654)
+for i in range(0, numberOfDates):
+    date = generateRandomDate(startDate, endDate)
+    dates.append(datetime(date.year, date.month, date.day))
 
-# predictions = []
-# for date in dates:
-#     try:
-#         prediction = wd.forecast_of_tomorrow("mythenquai", date)[0]
-#         predictions.append((date, prediction))
+predictions = []
+for date in dates:
+    try:
+        prediction = wd.forecast_of_tomorrow("mythenquai", date)[0]
+        predictions.append((date, prediction))
     
-#     except:
-#         print("Target day: ", date, " couldn't be forecasted... skip")
+    except:
+        print("Target day: ", date, " couldn't be forecasted... skip")
     
 
 # #comparison
-# nextDateAlgorithm_mean = []
-# nearestNeighbourAlgorithm_mean = []
-# nextDateAlgorithm_std = []
-# nearestNeighbourAlgorithm_std = []
-# for prediction in predictions:
-#     targetDate = prediction[0]
-#     targetTimeRange =  (datetime(targetDate.year, targetDate.month, targetDate.day, 0, 0, 1), datetime(targetDate.year, targetDate.month, targetDate.day, 23, 59, 59)) 
-#     nearestNDate = prediction[1]
-#     print("nearest n date: ", nearestNDate)
-#     nearestNDateTimeRange =  (datetime(nearestNDate.year, nearestNDate.month, nearestNDate.day, 0, 0, 1), datetime(nearestNDate.year, nearestNDate.month, nearestNDate.day, 23, 59, 59)) 
-#     nextDate = targetDate + timedelta(days=1)
-#     nextDateTimeRange =  (datetime(nextDate.year, nextDate.month, nextDate.day, 0, 0, 1), datetime(nextDate.year, nextDate.month, nextDate.day, 23, 59, 59)) 
+nextDateAlgorithm_mean = []
+nearestNeighbourAlgorithm_mean = []
+nextDateAlgorithm_std = []
+nearestNeighbourAlgorithm_std = []
+for prediction in predictions:
+    targetDate = prediction[0]
+    targetTimeRange =  (datetime(targetDate.year, targetDate.month, targetDate.day, 0, 0, 1), datetime(targetDate.year, targetDate.month, targetDate.day, 23, 59, 59)) 
+    nearestNDate = prediction[1]
+    print("nearest n date: ", nearestNDate)
+    nearestNDateTimeRange =  (datetime(nearestNDate.year, nearestNDate.month, nearestNDate.day, 0, 0, 1), datetime(nearestNDate.year, nearestNDate.month, nearestNDate.day, 23, 59, 59)) 
+    nextDate = targetDate + relativedelta(years=-1)
+    nextDateTimeRange =  (datetime(nextDate.year, nextDate.month, nextDate.day, 0, 0, 1), datetime(nextDate.year, nextDate.month, nextDate.day, 23, 59, 59)) 
     
-#     data_target = wd.get_measurement(wd.Measurement.Air_temp, "mythenquai", targetTimeRange)
-#     data_nearestN = wd.get_measurement(wd.Measurement.Air_temp, "mythenquai", nearestNDateTimeRange)
-#     data_nextDate = wd.get_measurement(wd.Measurement.Air_temp, "mythenquai", nextDateTimeRange)
+    data_target = wd.get_measurement(wd.Measurement.Air_temp, "mythenquai", targetTimeRange)
+    data_nearestN = wd.get_measurement(wd.Measurement.Air_temp, "mythenquai", nearestNDateTimeRange)
+    data_nextDate = wd.get_measurement(wd.Measurement.Air_temp, "mythenquai", nextDateTimeRange)
     
-#     mean_temp_target = np.nanmean(data_target["air_temperature"].tolist())
-#     mean_temp_nearestN = np.nanmean(data_nearestN["air_temperature"].tolist())
-#     mean_temp_data_nextDate = np.nanmean(data_nextDate["air_temperature"].tolist())
+    mean_temp_target = np.nanmean(data_target["air_temperature"].tolist())
+    mean_temp_nearestN = np.nanmean(data_nearestN["air_temperature"].tolist())
+    mean_temp_data_nextDate = np.nanmean(data_nextDate["air_temperature"].tolist())
 
-#     std_temp_target = np.nanstd(data_target["air_temperature"].tolist())
-#     std_temp_nearestN = np.nanstd(data_nearestN["air_temperature"].tolist())
-#     std_temp_data_nextDate  = np.nanstd(data_nextDate["air_temperature"].tolist())
+    std_temp_target = np.nanstd(data_target["air_temperature"].tolist())
+    std_temp_nearestN = np.nanstd(data_nearestN["air_temperature"].tolist())
+    std_temp_data_nextDate  = np.nanstd(data_nextDate["air_temperature"].tolist())
 
-#     difference_mean_nearestN = np.abs(mean_temp_target - mean_temp_nearestN)
-#     difference_mean_nextDate = np.abs(mean_temp_target - mean_temp_data_nextDate)
+    difference_mean_nearestN = np.abs(mean_temp_target - mean_temp_nearestN)
+    difference_mean_nextDate = np.abs(mean_temp_target - mean_temp_data_nextDate)
 
-#     difference_std_nearestN = np.abs(std_temp_target - std_temp_nearestN)
-#     difference_std_nextDate = np.abs(std_temp_target - std_temp_data_nextDate)
+    difference_std_nearestN = np.abs(std_temp_target - std_temp_nearestN)
+    difference_std_nextDate = np.abs(std_temp_target - std_temp_data_nextDate)
 
-#     nearestNeighbourAlgorithm_mean.append(difference_mean_nearestN)
-#     nextDateAlgorithm_mean.append(difference_mean_nextDate)
-#     nearestNeighbourAlgorithm_std.append(difference_std_nearestN)
-#     nextDateAlgorithm_std.append(difference_std_nextDate)
+    nearestNeighbourAlgorithm_mean.append(difference_mean_nearestN)
+    nextDateAlgorithm_mean.append(difference_mean_nextDate)
+    nearestNeighbourAlgorithm_std.append(difference_std_nearestN)
+    nextDateAlgorithm_std.append(difference_std_nextDate)
 
-# print("Accuracy nearest neighbour (mean difference): +-", np.mean(nearestNeighbourAlgorithm_mean), " °C")
-# print("Accuracy next date (mean difference): +-", np.mean(nextDateAlgorithm_mean), " °C") 
-# print("Accuracy nearest neighbour (standard deviation difference): +-", np.mean(nearestNeighbourAlgorithm_std), " °C")
-# print("Accuracy next date (standard deviation difference): +-", np.mean(nextDateAlgorithm_std), " °C") 
+print("Accuracy nearest neighbour (mean difference): +-", np.mean(nearestNeighbourAlgorithm_mean), " °C")
+print("Accuracy lastYear (mean difference): +-", np.mean(nextDateAlgorithm_mean), " °C") 
+print("Accuracy nearest neighbour (standard deviation difference): +-", np.mean(nearestNeighbourAlgorithm_std), " °C")
+print("Accuracy lastYear (standard deviation difference): +-", np.mean(nextDateAlgorithm_std), " °C")   """
 
-
+# response:
+# Accuracy nearest neighbour (mean difference): +- 1.9660088095189747  °C
+# Accuracy lastYear (mean difference): +- 3.643206346667881  °C
+# Accuracy nearest neighbour (standard deviation difference): +- 0.9623274555698359  °C
+# Accuracy lastYear (standard deviation difference): +- 1.113189511674036  °C
 
 
 

weather_app/weatherimport.py
@@ -1117,11 +1117,11 @@ def get_mean_of_day(df, measurements_converted, vector_lim_weight):
 
     mean_of_measurements_dateSearchFor_normalized_list.append(np.nanmean(df[measurement]) * weight)
 
-    pythoagoras = 0
-    for mean_measurement in mean_of_measurements_dateSearchFor_normalized_list:
-      pythoagoras += mean_measurement ** 2
+  pythoagoras = 0
+  for mean_measurement in mean_of_measurements_dateSearchFor_normalized_list:
+    pythoagoras += mean_measurement ** 2
 
-    pythoagoras = np.sqrt(pythoagoras)
+  pythoagoras = np.sqrt(pythoagoras)
 
   return pythoagoras
 
@@ -1196,20 +1196,20 @@ def nearest_neighbour(station: str, date_searchBestRecord: datetime, timeArea_mo
     diff_means.append(diffMean_of_hist_day_norm) #add value
 
 
-    if not best_meanDifference: #if no value stored
+    if best_meanDifference == None: #if no value stored
       best_meanDifference = diffMean_of_hist_day_norm
 
     elif diffMean_of_hist_day_norm < best_meanDifference:
       best_meanDifference = diffMean_of_hist_day_norm
 
-    if not worst_meanDifference: #if no value stored
+    if worst_meanDifference == None: #if no value stored
       worst_meanDifference = diffMean_of_hist_day_norm
 
-    elif diffMean_of_hist_day_norm > best_meanDifference:
+    elif diffMean_of_hist_day_norm > worst_meanDifference:
       worst_meanDifference = diffMean_of_hist_day_norm
   
   meanDifference_range = worst_meanDifference - best_meanDifference #calculate range
-  min_meanDifference = ((meanDifference_range / 100) * mean_in_range_percent) + best_meanDifference #mean of historical date has to be in range of meanDifference_range / 10 
+  min_meanDifference = ((meanDifference_range / 100) * mean_in_range_percent) + best_meanDifference #mean of historical date has to be in range of meanDifference_range / 100 
   
 
   #progress counter